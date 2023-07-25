429a6e5bbcc5065c24e3b634e055063f9e5600ea
crownVic0285 <vpcorona85@gmail.com>
2023-07-25 15:41:30 -0700

    Tweet Function added

---
+++ App.js
@@ -0,0 +1,24 @@
+function App(){
+    return (
+        <div>
+            <Tweet
+                name="John Wick"
+                username="JohnWick"
+                date={new Date().toDateString()}
+                message="Who killed my dog?!"
+            />
+            <Tweet
+                name="Elon Musk"
+                username="ElonMusk"
+                date={new Date().toDateString()}
+                message="I'm going to Mars!"
+            />
+            <Tweet
+                name="Jeff Bezos"
+                username="JeffBezos"
+                date={new Date().toDateString()}
+                message="Amazon is going to the moon!"
+            />
+        </div>
+    );
+}
